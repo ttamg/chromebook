e2da7e6334ebf65f93eeb4146ebaed24154a3712
Matt Gosden <mdgosden@gmail.com>
2020-10-13 23:10:04 +0100

    Python installation script

--- README.md
+++ README.md
@@ -16,6 +16,26 @@ You use these scripts at your own risk. They are tested on my Google Pixelbook a
 
 This is quite a time-consuming task. My chromebook comes with Python 3.5 as standard. This is now getting a bit out of date.
 
+### Starting a new Python project and creating a Virtual Environment
+
+A single script to create a new virtual environment, and project folder for a new Python project
+
+```bash
+    ./python-project.sh
+```
+
+ADD GIF
+
+### Jupyter Notebook projects
+
+Chromebooks work pretty well for Jupyter projects. This short script sets up a new project folder
+
+### Post-Powerwash reinstall
+
+Reinstalling Python and other development tools after a Powerwash takes some time. These scripts make this very quick on my machine.
+
+** Warning - some of these installations are at a more basic level so it is possible that they will not work on all Chromebooks. This has been tested on a Google Pixelbook**
+
 ## Finally
 
 If you find this useful, then please give it a star (click on the Github star at the top right of the screen) so that others find this repo more easily.
---
+++ install-python.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -e
+
+echo "A script to install Python 3.7 on a Chromebook"
+echo
+echo "This will:"
+echo "1. Install prerequisites "
+echo "2. create a new python 3 virtual environment in that folder"
+echo "3. install Jupyter Lab files in that folder"
+echo "4. add a readme file in the project folder with instructions"
+echo
+echo "Use this script at your own risk.  It may not work on all Chromebooks or setups."
+echo "This has been tested on a clean powerwashed Google Pixelbook."
+echo "If a step fails, the script will stop."
+echo
+read -p "Press return to continue ... or Ctrl-C to cancel." START
+echo
+
+echo "Python versions I can install:"
+echo "1 = Python 3.9.0"
+echo "2 = Python 3.8.6"
+echo "3 = Python 3.7.9"
+echo "4 = Python 3.6.12"
+read -p "Select Python version to install (1-4): " CHOICE
+
+case $CHOICE in
+
+    1)
+        VERSION=3.9
+        RELEASE=3.9.0
+        URL=https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz
+    ;;
+
+    2)
+        VERSION=3.8
+        RELEASE=3.8.6
+        URL=https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
+    ;;
+
+    3)
+        VERSION=3.7
+        RELEASE=3.7.9
+        URL=https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+    ;;
+
+    4)
+        VERSION=3.6
+        RELEASE=3.6.12
+        URL=https://www.python.org/ftp/python/3.6.12/Python-3.6.12.tgz
+    ;;
+
+    *)
+    echo 
+    echo "$CHOICE is not a valid option.  Aborting."
+    exit
+    ;;
+esac
+
+echo
+echo "Will compile Python $RELEASE from the source-code ..."
+
+read -n 1 -p "Are you really sure you want to continue?  (Y/y): " CONFIRM
+echo
+if [ $CONFIRM != "Y" ] && [ $CONFIRM != "y" ]
+then
+    echo "Aborting. "
+    exit
+fi
+
+echo 
+echo "Installing pre-requisites ..."
+echo "You may be asked to input your administrator 'sudo' password (your password) ..."
+
+sudo apt-get install libffi-dev
+
+sudo apt-get install build-essential checkinstall
+
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+echo "Done."
+echo
+echo "Downloading Python $RELEASE from Python.org ..."
+
+[[ -d /usr/src ]] || sudo mkdir /usr/src
+cd /usr/src
+
+sudo wget $URL
+
+echo
+echo "Unpacking and compiling Python ... this may take some time ... "
+
+sudo tar xzf Python-$RELEASE.tgz
+
+cd Python-$RELEASE
+
+sudo ./configure --enable-optimizations
+
+sudo make altinstall
+
+echo
+echo "Python installed.  Checking installation ..."
+
+python$VERSION -V
+
+echo "You can start python by typing 'python$VERSION' from the command line."
+echo
+echo "Finished."
