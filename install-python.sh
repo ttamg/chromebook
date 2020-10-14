d82e3358db95b8967f83c07d1fc99d27e5d7cdc9
Matt Gosden <mdgosden@gmail.com>
2020-10-14 11:10:28 +0100

    Added curl dependency

--- install-python.sh
+++ install-python.sh
@@ -88,7 +88,7 @@ sudo apt-get update
 sudo apt-get install build-essential
 
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
+libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev curl
 
 echo "Done."
 echo
